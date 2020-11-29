5aa1f20dd6b85db0c2683d0e2815bcc2259201d1
Canarypwn <qinfr@shanghaitech.edu.cn>
2020-11-29 11:14:55 +0800

    update exp3.py

---
+++ physicsexp/exp3.py
@@ -0,0 +1,20 @@
+
+from physexp import *
+a = [7.748,
+7.750,
+7.753,
+7.758,
+7.751,
+7.749,
+7.750,
+7.750,
+7.749,
+7.755
+]
+print(standardDeviation(a))
+print(standardDeviationOfTheMean(a))
+
+a = [7.75,7.751,7.750,7.750]
+print(Avg(a))
+print(standardDeviation(a))
+print(standardDeviationOfTheMean(a))
